9f817329508a42967ab8b7ce3e46b49e2cf19250
Alice <nadellepav@gmail.com>
2026-03-04 13:18:48 -0600

    typo

--- IEEE_Robot_2026/controlArduino.py
+++ IEEE_Robot_2026/controlArduino.py
@@ -36,7 +36,7 @@ def move_motor_full(direction: int):
 
 def turn_servos(direction: int):
     if direction not in (0,2):
-        raise ValueError ("Direction must be 0 or +1 or +2 (in and out)");
+        raise ValueError ("Direction must be 0 or +1 or +2 (in and out)")
     ser.write(bytes([0xFF, 0x03, direction, 0x00]))
     ack = ser.read()
     if ack == b'\xAA':
